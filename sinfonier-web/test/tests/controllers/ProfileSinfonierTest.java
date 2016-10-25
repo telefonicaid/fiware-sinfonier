1732641a73ec0f5f81481e8394ab0eae3b410a2a
Alex Matos <alejandro.matos@global.11paths.com>
2016-10-25 10:10:15 +0200

    add real test for Profile sinfonier.

--- sinfonier-web/test/tests/controllers/ProfileSinfonierTest.java
+++ sinfonier-web/test/tests/controllers/ProfileSinfonierTest.java
@@ -6,6 +6,9 @@ import play.mvc.Http;
 import tests.BaseTestFunctional;
 import tests.TestData;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
 public class ProfileSinfonierTest extends BaseTestFunctional {
 
   private static Http.Request request;
@@ -17,9 +20,31 @@ public class ProfileSinfonierTest extends BaseTestFunctional {
     doMongoImport(TestData.MODULES_COLLECTION, TestData.MODULES_JSON_FILE);
   }
 
+  @Before
+  public void setUp() throws IOException, ParserConfigurationException {
+    request = doLogin("test@test.com", "test");
+  }
+
   @Test
   public void index() {
-    assertTrue(true);
+    Http.Response response = GET(request, "/profile");
+    assertIsOk(response);
+    assertContentType("text/html", response);
+    assertCharset(play.Play.defaultWebEncoding, response);
+  }
+
+  @Test
+  public void edit() {
+    Http.Response response = GET(request, "/profile/test@test.com/edit");
+    assertIsOk(response);
+    assertContentType("text/html", response);
+    assertCharset(play.Play.defaultWebEncoding, response);
+  }
+
+  @After
+  public void tearDown() {
+    doLogout();
+    request = null;
   }
 
   @AfterClass
