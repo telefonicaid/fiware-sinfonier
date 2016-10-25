7497a4fa901f5c9dace1741e24b009054d05bd3f
Alex Matos <alejandro.matos@global.11paths.com>
2016-10-25 08:39:42 +0200

    Diagnosis test doesn't need to login for working

--- sinfonier-web/test/tests/controllers/DiagnosisTest.java
+++ sinfonier-web/test/tests/controllers/DiagnosisTest.java
@@ -10,19 +10,15 @@ import tests.TestData;
 
 public class DiagnosisTest extends BaseTestFunctional {
 
-  private static Http.Request request;
-
   @BeforeClass
   public static void setUp() throws Exception {
-    doMongoImport(TestData.USERS_COLLECTION, TestData.USERS_JSON_FILE);
     doMongoImport(TestData.MODULE_VERSIONS_COLLECTION, TestData.MODULE_VERSIONS_JSON_FILE);
     doMongoImport(TestData.MODULES_COLLECTION, TestData.MODULES_JSON_FILE);
-    request = doLogin("test@test.com", "test");
   }
 
   @Test
   public void index() {
-    Http.Response response = GET(request, "/diagnosis");
+    Http.Response response = GET(newRequest(), "/diagnosis");
     assertIsOk(response);
     assertContentType("text/html", response);
     assertCharset(play.Play.defaultWebEncoding, response);
@@ -30,8 +26,6 @@ public class DiagnosisTest extends BaseTestFunctional {
 
   @AfterClass
   public static void tearDown() throws Exception {
-    doLogout();
-    doMongoDrop(TestData.USERS_COLLECTION);
     doMongoDrop(TestData.MODULES_COLLECTION);
     doMongoDrop(TestData.MODULE_VERSIONS_COLLECTION);
   }
