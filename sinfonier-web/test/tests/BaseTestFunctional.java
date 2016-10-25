30ab69c373063de0c5d6107f9f848c8d869825f9
Alex Matos <amatosc@users.noreply.github.com>
2016-10-25 16:32:01 +0200

    [Test] Create UnitTest and FuncionalTest. #19414. (#8)
    * [Test] Create UnitTest and FuncionalTest. #19414
    
    * DiagnosisTest, DrawerTest and ModulesTest
    
    * Remove unnecessary imports
    
    * Add some new topologies for doing test
    
    * TopologiesTests
    
    * TopologiesTests
    
    * Diagnosis test doesn't need to login for working
    
    * add real test for Profile sinfonier.

--- sinfonier-web/test/tests/BaseTestFunctional.java
+++ sinfonier-web/test/tests/BaseTestFunctional.java
@@ -1,18 +1,61 @@
 package tests;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.Ignore;
+import play.mvc.Http;
 import play.test.FunctionalTest;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
-@Ignore public class BaseTestFunctional extends FunctionalTest {
+@Ignore
+public class BaseTestFunctional extends FunctionalTest {
+
+  private static Http.Cookie sessionToken;
 
   protected static void doMongoImport(String collectionName, String jsonFilePath) throws UnknownHostException, IOException {
     BaseTest.doMongoImport(collectionName, jsonFilePath);
   }
-  
+
   protected static void doMongoDrop(String collectionName) {
     BaseTest.doMongoDrop(collectionName);
+  }
+
+  protected static Http.Request doLogin(String user, String password) throws IOException, ParserConfigurationException {
+    if (sessionToken == null) {
+      Http.Response login = GET(newRequest(), "/login");
+      String authenticityToken = getAuthenticityTokenLogin(login);
+      Http.Response auth = POST("/login?username=" + user + "&password=" + password + "&authenticityToken=" + authenticityToken);
+      sessionToken = auth.cookies.get("SINFONIER_SESSION");
+    }
+
+    Http.Request r = newRequest();
+    r.cookies.put(sessionToken.name, sessionToken);
+
+    return r;
+  }
+
+  protected static void doLogout() {
+    Http.Response dashboard = GET(newRequest(), "/dashboard");
+    String authenticityToken = getAuthenticityTokenLogout(dashboard);
+    POST("/logout?authenticityToken=" + authenticityToken);
+    sessionToken = null;
+  }
+
+  private static String getAuthenticityTokenLogin(Http.Response res) {
+    Document doc = Jsoup.parse(res.out.toString());
+    Element form = doc.getElementsByAttributeValue("action", "/login").get(0);
+    Element input = form.getElementsByAttributeValue("name", "authenticityToken").get(0);
+    return input.val();
+  }
+
+  private static String getAuthenticityTokenLogout(Http.Response res) {
+    Document doc = Jsoup.parse(res.out.toString());
+    Element form = doc.getElementsByAttributeValue("action", "/logout").get(0);
+    Element input = form.getElementsByAttributeValue("name", "authenticityToken").get(0);
+    return input.val();
   }
 }
---
+++ sinfonier-web/test/tests/controllers/DashboardTest.java
@@ -0,0 +1,39 @@
+package tests.controllers;
+
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import play.mvc.Http;
+import tests.BaseTestFunctional;
+import tests.TestData;
+
+public class DashboardTest extends BaseTestFunctional {
+
+  private static Http.Request request;
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+    doMongoImport(TestData.USERS_COLLECTION, TestData.USERS_JSON_FILE);
+    doMongoImport(TestData.MODULE_VERSIONS_COLLECTION, TestData.MODULE_VERSIONS_JSON_FILE);
+    doMongoImport(TestData.MODULES_COLLECTION, TestData.MODULES_JSON_FILE);
+    request = doLogin("test@test.com", "test");
+  }
+
+  @Test
+  public void index() {
+    Http.Response response = GET(request, "/dashboard");
+    assertIsOk(response);
+    assertContentType("text/html", response);
+    assertCharset(play.Play.defaultWebEncoding, response);
+  }
+
+
+  @AfterClass
+  public static void tearDown() throws Exception {
+    doLogout();
+    doMongoDrop(TestData.USERS_COLLECTION);
+    doMongoDrop(TestData.MODULES_COLLECTION);
+    doMongoDrop(TestData.MODULE_VERSIONS_COLLECTION);
+  }
+}
