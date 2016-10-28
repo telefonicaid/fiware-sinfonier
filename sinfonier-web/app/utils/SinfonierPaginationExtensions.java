0b47fb8908c4e01b317f069c5bf7e071ffaaa2ce
fandrod <fandrod@aspgems.com>
2016-10-28 14:26:25 +0200

    [Resolves #20206] Improve pagination on modules and topologies lists (#15)
    * [Resolves #20206] Improve pagination on modules and topologies lists
    
    * [Resolves #20206] Code review

--- sinfonier-web/app/utils/SinfonierPaginationExtensions.java
+++ sinfonier-web/app/utils/SinfonierPaginationExtensions.java
@@ -2,30 +2,33 @@ package utils;
 
 import ext.PaginationExtensions;
 
+import play.i18n.Messages;
 import java.util.*;
 
 public class SinfonierPaginationExtensions extends PaginationExtensions {
 
-  public static List<Integer> buildPossiblePages(int page, int results, int pageSize) {
+  public static List<PageElement> buildPossiblePages(int page, int results, int pageSize) {
     int totalPages = Math.max(1, (int) Math.ceil((double) results / pageSize));
-    List<Integer> pages = new ArrayList<Integer>();
+    List<PageElement> pages = new ArrayList<PageElement>();
     if (page == 1) {
       for (int i = page; i <= Math.min(page + 2, totalPages); i++) {
-        pages.add(i);
+        pages.add(new PageElement(i,Integer.toString(i)));
       }
     } else if (page == totalPages) {
       for (int i = Math.max(totalPages - 2, 1); i <= totalPages; i++) {
-         pages.add(i);
+         pages.add(new PageElement(i,Integer.toString(i)));
       }
     } else {
       for (int i = page - 1; i <= Math.min(page + 1, totalPages); i++) {
-        pages.add(i);
+        pages.add(new PageElement(i,Integer.toString(i)));
       }
     }
-    if (pages.size() == 3 && pages.get(2) < totalPages) {
-      pages.add(totalPages);
+    if (pages.size() == 3 && pages.get(2).getPageNumber() < totalPages) {
+      pages.add(new PageElement(totalPages, Messages.get("Utils.pagination.last")));
+    }
+    if (pages.size() >= 3 && pages.get(0).getPageNumber() > 1) {
+      pages.add(0, new PageElement(1, Messages.get("Utils.pagination.first")));
     }
     return pages;
   }
-
 }
