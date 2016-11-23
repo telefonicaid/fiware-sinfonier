5b1aca5e262262cdbb0f7e285c53bde5d63a78b1
alberto.sanchez <alberto.sanchez@11paths.com>
2016-11-23 15:13:56 +0100

    Reverse code changed by error

--- sinfonier-backend-api/clustering/storm/TopologyBuilder.py
+++ sinfonier-backend-api/clustering/storm/TopologyBuilder.py
@@ -48,11 +48,11 @@ class TopologyBuilder(object):
             _module_version_code = module[ModuleConsts.FIELD_VERSION_CODE]
 
             if not ids.get(_module_id):
-                ids.setdefault(module[ModuleConsts.FIELD_MODULE_ID], [_module_version_code])
-            elif not ids.get(_module_id) and not ids.__contains__(_module_version_code):
+                ids.setdefault(_module_id, [_module_version_code])
+            elif ids.get(_module_id) and not ids.get(_module_id).__contains__(_module_version_code):
                 _module_versions_codes = ids.get(_module_id)
                 _module_versions_codes.append(_module_version_code)
-                ids.setdefault(module[ModuleConsts.FIELD_MODULE_ID], _module_versions_codes)
+                ids.setdefault(_module_id, _module_versions_codes)
 
         _modules = MongodbFactory.get_modules(ids.keys())
 
@@ -202,7 +202,7 @@ class TopologyBuilder(object):
 
                 mod_info["language"] = module["language"]
                 mod_info["abstractionId"] = "{module_name}_{random_id}".format(module_name=module["name"], random_id=uuid.uuid4())
-                mod_info["parallelism"] = module["parallelism"] if "parallelism" in module else "1"
+                mod_info["parallelism"] = module["parallelisms"] if "parallelisms" in module else "1"
                 mod_info["language"] = module["language"]
                 mod_info["sources"] = list()
                 mod_info["params"] = dict()
