11c2060a1d2c639f91cb987ffba88dfcb2331a63
Alex Matos <amatosc@users.noreply.github.com>
2017-01-03 13:25:11 +0100

    Add EULA for user creation. #21665 (#64)

--- sinfonier-web/app/models/factory/SinfonierFactory.java
+++ sinfonier-web/app/models/factory/SinfonierFactory.java
@@ -1,6 +1,5 @@
 package models.factory;
 
-import models.user.MongoUser;
 import models.user.SinfonierUser;
 import models.user.User;
 
@@ -9,8 +8,8 @@ import com.mongodb.DBObject;
 public class SinfonierFactory extends MongoFactory {
 
   @Override
-  public User buildUser(String name, String email, String password) {
-      return new User(new SinfonierUser(name, email, password));
+  public User buildUser(String name, String email, String password, Boolean eula) {
+      return new User(new SinfonierUser(name, email, password, eula));
   }
 
   @Override
