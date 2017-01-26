454558e8994e26b6525219b086db8e9bc3c51ff5
Iván Ramos Muñoz <ivan.ramos@11paths.com>
2017-01-26 16:33:18 +0100

    Implement OAuth authentication

---
+++ sinfonier-web/app/controllers/OAuthUserModel.java
@@ -0,0 +1,32 @@
+package controllers;
+
+public class OAuthUserModel {
+
+    private String id;
+    private String displayName;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
